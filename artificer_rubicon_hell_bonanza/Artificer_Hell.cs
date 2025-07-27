4474be58414b5b2a0f6f3acfb48cbbeb0dc5dad4
danxnad <example@mail.com>
2025-07-27 15:21:24 +0400

    forgot to update the mod version

--- artificer_rubicon_hell_bonanza/Artificer_Hell.cs
+++ artificer_rubicon_hell_bonanza/Artificer_Hell.cs
@@ -17,7 +17,7 @@ using RewiredConsts;
 
 namespace artificer_rubicon_hell_bonanza
 {
-    [BepInPlugin("dannad.lockeveryroom", "Artificer Rubicon Hell Bonanza", "0.0.3")]
+    [BepInPlugin("dannad.lockeveryroom", "Artificer Rubicon Hell Bonanza", "0.0.31")]
     public class Artificer_Hell : BaseUnityPlugin
     {
         public Player player;
